4132ead7a7ae2783a7d68502595d63a67f2bef08
Un1q32 <joey.t.reinhart@gmail.com>
2024-12-17 11:02:10 -0500

    update llvm version

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ cp -a "$scriptroot"/files/* "$pwd/iphoneports-toolchain"
 mkdir "$scriptroot/build" && cd "$scriptroot/build" || exit 1
 
 printf "Building LLVM+Clang\n\n"
-llvmver="19.1.5"
+llvmver="19.1.6"
 curl -# -L "https://github.com/llvm/llvm-project/releases/download/llvmorg-$llvmver/llvm-project-$llvmver.src.tar.xz" | tar -xJ
 mkdir "llvm-project-$llvmver.src/build"
 (
