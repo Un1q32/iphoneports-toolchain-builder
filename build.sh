7a8758b0653aeeab30ae3a109749aa044d2e1984
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-03 00:27:58 -0500

    tweak

--- build.sh
+++ build.sh
@@ -135,7 +135,7 @@ for src in $x64srcs; do
     done
     "$clang" -isysroot "$scriptroot/src/macsysroot" -target x86_64-apple-macos10.4 "../compiler-rt/lib/builtins/$src" -c -O3 -o "x86_64-${src%\.c}.o" &
 done
-"$clang" -isysroot "$scriptroot/src/macsysroot" -target arm64-apple-macos11.0 -xc /dev/null -c -O3 -o arm64-nothing.o &
+"$clang" -isysroot "$scriptroot/src/macsysroot" -target arm64-apple-macos11.0 -xc /dev/null -c -O3 -o nothing.o &
 wait
 
 "$pwd/iphoneports-toolchain/share/iphoneports/cctools-bin/libtool" -static -o libclang_rt.osx.a ./*.o 2>/dev/null
