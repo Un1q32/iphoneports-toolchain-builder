d1fba114d3ca2f08178527700c67f627aba53e04
Un1q32 <joey.t.reinhart@gmail.com>
2025-02-23 00:25:50 -0500

    fix

--- build.sh
+++ build.sh
@@ -5,10 +5,9 @@ case $JOBS in
     ''|*[!0-9]*)
         if command -v nproc > /dev/null; then
             cpus=$(nproc)
-        elif sysctl -n hw.ncpu > /dev/null 2>&1; then
-            cpus=$(sysctl -n hw.ncpu)
         else
-            cpus=1
+            cpus=$(sysctl -n hw.ncpu 2> /dev/null)
+            [ -z "$cpus" ] && cpus=1
         fi
 
         JOBS=$((cpus * 2 / 3))
