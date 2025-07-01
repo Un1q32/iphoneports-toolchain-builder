2740b565d52e4b719f4483c36ab4c7373c72b64e
Un1q32 <joey.t.reinhart@gmail.com>
2025-06-30 20:28:55 -0400

    fix rust build

--- build.sh
+++ build.sh
@@ -177,9 +177,8 @@ case $host in
     (x86_64-*-linux-musl) host=x86_64-unknown-linux-musl ;;
 esac
 printf "Building rust\n\n"
-# rustver="1.88.0"
-# curl -# -L "https://static.rust-lang.org/dist/rustc-${rustver}-src.tar.xz" | tar xJ
-curl -# -L "https://static.rust-lang.org/dist/rustc-beta-src.tar.xz" | tar xJ
+rustver="1.88.0"
+curl -# -L "https://static.rust-lang.org/dist/rustc-${rustver}-src.tar.xz" | tar xJ
 (
 cd rustc-*/
 sed -e "s|@PREFIX@|$pwd/iphoneports-toolchain/share/iphoneports|g" -e "s|@LLVMCONFIG@|$pwd/iphoneports-toolchain/share/iphoneports/bin/llvm-config|g" -e "s|@HOST@|$host|g" "$scriptroot/src/bootstrap.toml" > bootstrap.toml
